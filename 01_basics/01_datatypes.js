6ee52d0dd572acf33d53c1e7ec3b1782c0d2871c
Mohit rawat <mr746167@gmail.com>
2026-09-13 15:10:44 +0000

    data types

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+// Data Types in JS
+
+"use strict";  // treat all JS code as newer version
+
+let name = "Mohit"
+let age = 20;
+let isLoggedIn = true
+
+console.log(name," ",age," ",isLoggedIn);
+console.log(typeof isLoggedIn);   // boolean
+console.log(typeof undefined);   // undefined
+console.log(typeof null);   // object
+ 
+// number
+// bigint
+// string
+// boolean -> true/false
+// null -> standalone value
+// undefined -> value not assigned
+// symbol -> unique
+
+// object
